c808b625a515cc88aa7990e96be3b0103b9749b6
ym <bruh@ez.pz>
2021-02-14 00:08:30 +0900

    - nigami fix

--- src/main/java/com/wurst/wurstclient/module/impl/BuildRandom.java
+++ src/main/java/com/wurst/wurstclient/module/impl/BuildRandom.java
@@ -66,26 +66,30 @@ public class BuildRandom extends Module {
     public boolean placeBlock(BlockPos pos) {
         Vec3d eyesPos = new Vec3d(mc.player.posX, mc.player.posY + mc.player.getEyeHeight(), mc.player.posZ);
         final Vec3d posVec = new Vec3d(pos).addVector(0.5, 0.5, 0.5);
+        final double distanceSqPosVec = eyesPos.squareDistanceTo(posVec);
 
         for (EnumFacing facing : EnumFacing.values()) {
             final BlockPos neighbor = pos.offset(facing);
 
             // Check if it can be placed.
             if (mc.world.getBlockState(neighbor).getBlock().canCollideCheck(mc.world.getBlockState(pos), false)) {
-                final Vec3d hitVec = posVec.add(new Vec3d(facing.getDirectionVec()).scale(0.5));
-                if (eyesPos.squareDistanceTo(hitVec) <= 36.0) {
-                    if (mc.world.rayTraceBlocks(eyesPos, hitVec, false, true, false) == null) {
-                        float[] rotations = getNeededRotations(hitVec);
-                        // ServerSide Rotation
-                        mc.player.connection.sendPacket(new CPacketPlayer.Rotation(rotations[0], rotations[1], mc.player.onGround));
+                final Vec3d dirVec = new Vec3d(facing.getDirectionVec());
+                final Vec3d hitVec = posVec.add(dirVec.scale(0.5));
+                if (eyesPos.squareDistanceTo(hitVec) <= 18.0625) {
+                    if (distanceSqPosVec <= eyesPos.squareDistanceTo(posVec.add(dirVec))) {
+                        if (mc.world.rayTraceBlocks(eyesPos, hitVec, false, true, false) == null) {
+                            float[] rotations = getNeededRotations(hitVec);
+                            // ServerSide Rotation
+                            mc.player.connection.sendPacket(new CPacketPlayer.Rotation(rotations[0], rotations[1], mc.player.onGround));
 
-                        // Click
-                        mc.playerController.processRightClickBlock(mc.player, mc.world, neighbor, facing.getOpposite(), hitVec, EnumHand.MAIN_HAND);
+                            // Click
+                            mc.playerController.processRightClickBlock(mc.player, mc.world, neighbor, facing.getOpposite(), hitVec, EnumHand.MAIN_HAND);
 
-                        // Send Swing packets
-                        mc.player.connection.sendPacket(new CPacketAnimation(EnumHand.MAIN_HAND));
-                        this.delay = 2.5F;
-                        return true;
+                            // Send Swing packets
+                            mc.player.connection.sendPacket(new CPacketAnimation(EnumHand.MAIN_HAND));
+                            this.delay = 2.5F;
+                            return true;
+                        }
                     }
                 }
             }
